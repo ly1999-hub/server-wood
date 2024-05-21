bcec40a40da85dbadccece3d1bcb6509b8ce9a59
ly1999-hub <nhly123123456789@gmail.com>
2024-05-21 13:18:48 +0700

    first commit

---
+++ .venv/helloworld.py
@@ -0,0 +1,65 @@
+from flask import Flask, render_template
+from flask import Flask, request, jsonify
+import requests
+import numpy as np
+from PIL import Image
+
+app = Flask(__name__)
+         
+@app.route('/home')
+def home():
+    return render_template('home.html')
+
+@app.route('/check-wood', methods=['POST'])
+def hello_world():
+    file = request.files['image']
+    img = Image.open(file)
+
+    img_prev = img.resize((224,224))
+    img_prev_array = np.array(img_prev) / 255.0
+    img_prev_array = np.expand_dims(img_prev_array, axis=0)
+    server_prev_url='http://localhost:8601/v1/models/wood1:predict'
+    response_prev = requests.post(server_prev_url, json={"instances": img_prev_array.tolist()})
+    prediction_prev=response_prev.json()
+    res_prev =prediction_prev['predictions'][0]
+    max_value=np.max(res_prev)
+    if max_value==res_prev[2]:
+        img = img.resize((50, 50))
+        img_array = np.array(img) / 255.0  # Chuẩn hóa giá trị pixel từ 0-255 thành 0-1
+        img_array = np.expand_dims(img_array, axis=0)
+        # Gửi ảnh đến TensorFlow Serving để thực hiện dự đoán
+        server_url = 'http://localhost:8501/v1/models/wood:predict'  # Thay đổi địa chỉ này thành địa chỉ của TensorFlow Serving
+        response = requests.post(server_url, json={"instances": img_array.tolist()})
+        prediction = response.json()
+        print(response)
+        labs=["Gỗ gió bầu","Gỗ Bạch đàn","Gỗ Lim","Gỗ Sồi","Gỗ thông","Gỗ Trắc(gỗ cầm lai)","Go Tràm","Go xoan"]
+        res =prediction['predictions'][0]
+        dbs = [round(num*100, 2) for num in res]
+        for i in range(len(dbs)):
+            for j in range(i+1,len(dbs)):
+                if dbs[i]<dbs[j]:
+                    tam = dbs[i]
+                    dbs[i]=dbs[j]
+                    dbs[j]=tam
+
+                    temp = labs[i]
+                    labs[i]=labs[j]
+                    labs[j]=temp
+        #return render_template('postImage.html',dbs=dbs,labs=labs)
+        
+        resp=[dbs,labs]
+        return resp
+    return "Not wood"
+
+@app.route('/phone')
+def get_phone():
+    return render_template('phone.html')
+
+@app.route('/check-wood')
+def showSignUp():
+    return render_template('postImage.html')
+    
+if __name__ == '__main__':
+    app.run(port=5000)
+    
+# http://localhost:5000/check-wood
