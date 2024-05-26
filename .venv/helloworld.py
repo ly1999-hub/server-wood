76932954f8866c6372cd2a59cf69efda1c019b08
ly1999-hub <nhly123123456789@gmail.com>
2024-05-26 14:00:39 +0700

    update

--- .venv/helloworld.py
+++ .venv/helloworld.py
@@ -18,7 +18,7 @@ def hello_world():
     img_prev = img.resize((224,224))
     img_prev_array = np.array(img_prev) / 255.0
     img_prev_array = np.expand_dims(img_prev_array, axis=0)
-    server_prev_url='http://localhost:8601/v1/models/wood1:predict'
+    server_prev_url='http://192.168.1.4:8601/v1/models/wood1:predict'
     response_prev = requests.post(server_prev_url, json={"instances": img_prev_array.tolist()})
     prediction_prev=response_prev.json()
     res_prev =prediction_prev['predictions'][0]
@@ -28,26 +28,27 @@ def hello_world():
         img_array = np.array(img) / 255.0  # Chuẩn hóa giá trị pixel từ 0-255 thành 0-1
         img_array = np.expand_dims(img_array, axis=0)
         # Gửi ảnh đến TensorFlow Serving để thực hiện dự đoán
-        server_url = 'http://localhost:8501/v1/models/wood:predict'  # Thay đổi địa chỉ này thành địa chỉ của TensorFlow Serving
+        server_url = 'http://192.168.1.4:8501/v1/models/wood:predict'  # Thay đổi địa chỉ này thành địa chỉ của TensorFlow Serving
         response = requests.post(server_url, json={"instances": img_array.tolist()})
         prediction = response.json()
         print(response)
         labs=["Gỗ gió bầu","Gỗ Bạch đàn","Gỗ Lim","Gỗ Sồi","Gỗ thông","Gỗ Trắc(gỗ cầm lai)","Go Tràm","Go xoan"]
         res =prediction['predictions'][0]
-        dbs = [round(num*100, 2) for num in res]
-        for i in range(len(dbs)):
-            for j in range(i+1,len(dbs)):
-                if dbs[i]<dbs[j]:
-                    tam = dbs[i]
-                    dbs[i]=dbs[j]
-                    dbs[j]=tam
+        array = [round(num*100, 2) for num in res]
+        for i in range(len(array)):
+            for j in range(i+1,len(array)):
+                if array[i]<array[j]:
+                    tam = array[i]
+                    array[i]=array[j]
+                    array[j]=tam
 
                     temp = labs[i]
                     labs[i]=labs[j]
                     labs[j]=temp
         #return render_template('postImage.html',dbs=dbs,labs=labs)
-        
-        resp=[dbs,labs]
+        dbs = [x for x in array if x >= 3]
+
+        resp=[dbs,labs[0:len(dbs)]]
         return resp
     return "Not wood"
 
